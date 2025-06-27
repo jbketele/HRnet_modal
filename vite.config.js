b2272324b4e9fd7c5124a095b5552e4aa983f656
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2025-06-27 10:18:15 +0200

    error modal ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        dts({ insertTypesEntry: true }) // pour générer index.d.ts
+    ],
     build: {
         lib: {
             entry: './src/index.js',
